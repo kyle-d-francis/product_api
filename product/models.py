d427cc8a6dd670c84aa8c2c9e847d084a3ed7fd1
kyle-d-francis <102608450+kyle-d-francis@users.noreply.github.com>
2022-04-14 13:07:55 -0500

    added models
    added the models to the product class

--- product/models.py
+++ product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
